a3298092695d3b285b329b652d53a5ec89ab9a92
Anthony Potts <anthonypotts@Anthonys-MacBook-Pro-2.local>
2021-01-21 14:48:39 -0600

    fixed error at Tag belongsToMany, 'as' needed new name

--- models/index.js
+++ models/index.js
@@ -25,7 +25,7 @@ Product.belongsToMany(Tag, {
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: 'product_tag',
+  as: 'product_tags',
   foreignKey: 'tag_id'
 })
 
