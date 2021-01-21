602ccf7833caedac6b3af289f2cb0fee95b0899e
Anthony Potts <anthonypotts@Anthonys-MacBook-Pro-2.local>
2021-01-20 20:42:47 -0600

    created and defined Model tables and model/index.js

--- models/index.js
+++ models/index.js
@@ -3,18 +3,30 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { getEnabledCategories } = require('trace_events');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'tagged_products',
+  foreignKey: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'product_tag',
+  foreignKey: 'tag_id'
+})
 
-module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
-};
+module.exports = { Product, Category, Tag, ProductTag };
